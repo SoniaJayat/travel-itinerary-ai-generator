5eba8d6ac8b79334473c8d9a28c508ed19858fb4
SoniaJayat <SoniaJayat03@gmail.com>
2024-01-30 00:49:50 +0000

    Improved the User Experience (UX)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayHoliday(response) {
-  console.log("holiday generated");
   new Typewriter("#holiday", {
     strings: response.data.answer,
     autoStart: true,
@@ -15,12 +14,18 @@ function generateHoliday(event) {
   let apiKey = "2fe0035acto21f1ea33794b41270a58f";
   let prompt = `Generate a travel itinerary for ${instructionsInput.value}`;
   let context =
-    "Always display a HTML title of the city and country that is big, bold and on its own line. You are a travel expert who loves providing quick days out with food suggestions. Generate a short brief that is only 3 lines about what the country has to offer and is not bold. After the description provide the user with what they can do for 3 days in the city/country in bullet points with a short description that is easy to read. Put the description as its own paragraph. Make the days bold and present them on their own line with the activities following in bullet points. Please do not state the words title, description or anything with plan/itinerary. Only have one h1. Please finish sentences. Days have to be a title. Please present the same layout for every city/country.";
+    "Always display a HTML title of the city and country that is big, bold and on its own line. You are a travel expert who loves providing quick days out with food suggestions. Generate a short brief that is only 3 lines about what the country has to offer and is not bold. After the description provide the user with what they can do for 3 days in the city/country in bullet points with a short description that is easy to read. Put the description as its own paragraph. Make the days bold and present them on their own line as titles with the activities following in bullet points. Please do not state the words title, description or anything with plan/itinerary. Only have one h1. Please finish sentences. Days have to be h2. You must be consistent and present the same layout every time.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating your itinerary");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let holidayElement = document.querySelector("#holiday");
+  holidayElement.classList.remove("hidden");
+  new Typewriter("#holiday", {
+    strings: ["⏳ Preparing your itinerary"],
+    autoStart: true,
+    delay: 100,
+    loop: true,
+    pauseFor: 1000,
+  });
 
   axios.get(apiUrl).then(displayHoliday);
 }
